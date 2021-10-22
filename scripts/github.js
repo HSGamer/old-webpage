ff3a364010df1c93ed4aaca8e12f707431f67943
Huynh Tien <huynhqtienvtag@gmail.com>
2021-10-22 08:34:26 +0700

    github.js as standalone and js and css

--- scripts/github.js
+++ scripts/github.js
@@ -1,35 +1,16 @@
-let url = new URL("https://api.github.com/users/hsgamer/repos");
-let params = {
+async function fetchRepos(url, params = {
     sort: "created",
     per_page: "100"
-};
-url.search = new URLSearchParams(params).toString();
-
-fetch(url)
-.then(data => data.json())
-.then(data => {
-    let projects = document.getElementById("projects");
-    data.forEach(repo => {
-        if (!repo.fork && !repo.archived) {
-            let anchor = document.createElement("a");
-            anchor.href = repo.html_url;
-
-            let project = document.createElement("div");
-            project.classList.add("project");
-
-            let projectName = document.createElement("h2");
-            projectName.innerHTML = repo.full_name + " (" + repo.stargazers_count + ")";
-            projectName.classList.add("project-name");
-            project.appendChild(projectName);
-
-            let projectDescrption = document.createElement("p");
-            projectDescrption.innerHTML = repo.description || "No Description";
-            projectDescrption.classList.add("project-description");
-            project.appendChild(projectDescrption);
-
-            anchor.appendChild(project);
-            projects.appendChild(anchor);
-            console.log(repo);
-        }
-    });
-});
+}) {
+    let urlObj = url instanceof URL ? url : new URL(url);
+    urlObj.search = new URLSearchParams(params).toString();
+    let repos = [];
+    await fetch(urlObj).then(data => data.json()).then(data => {
+        data.forEach(repo => {
+            if (!repo.fork && !repo.archived) {
+                repos.push(repo);
+            } 
+        });
+    })
+    return repos;
+}
