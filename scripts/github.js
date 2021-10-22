1f450893bcb587d94dcacabea45ada03ebc33616
Huynh Tien <huynhqtienvtag@gmail.com>
2021-10-22 10:43:36 +0700

    load all public orgs

--- scripts/github.js
+++ scripts/github.js
@@ -1,16 +1,17 @@
-async function fetchRepos(url, params = {
-    sort: "created",
+async function fetchRepos(user, includeOrgRepos = true, params = {
     per_page: "100"
 }) {
-    let urlObj = url instanceof URL ? url : new URL(url);
-    urlObj.search = new URLSearchParams(params).toString();
     let repos = [];
-    await fetch(urlObj).then(data => data.json()).then(data => {
-        data.forEach(repo => {
-            if (!repo.fork && !repo.archived) {
-                repos.push(repo);
-            } 
+    let urls = [ new URL("https://api.github.com/users/" + user + "/repos") ];
+    if (includeOrgRepos) {
+        let orgUrl = "https://api.github.com/users/" + user + "/orgs";
+        await fetch(orgUrl).then(data => data.json()).then(data => {
+            data.forEach(org => urls.push(new URL(org.repos_url)));
         });
-    })
+    }
+    urls.forEach(url => url.search = new URLSearchParams(params).toString());
+    let promises = [];
+    urls.forEach(url => promises.push(fetch(url).then(data => data.json())));
+    await Promise.all(promises).then(data => data.forEach(repo => repos.push(...repo)));
     return repos;
 }
